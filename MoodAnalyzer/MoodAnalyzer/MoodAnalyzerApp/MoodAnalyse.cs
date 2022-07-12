ec3cb3751c304e2fd5fc1367ac7787d7fc29011b
Balla <ballaphanindra@outlook.com>
2022-07-12 11:09:31 +0530

    Updating UC1,TC1.1,TC1.2

---
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyzerApp/MoodAnalyse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MoodAnalyzer.MoodAnalyzerApp
+{
+    internal class MoodAnalyse
+    {
+        private string message;
+
+        /// <summary>
+        /// Parameterised Constructor.
+        /// </summary>
+        /// <param name="message"></param>
+        public MoodAnalyse(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            if (this.message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
